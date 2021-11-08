2bb4397deb07d693a0c41aa7d586b702a6a79e52
LihaoYin <86851749+LihaoYin@users.noreply.github.com>
2021-11-08 00:35:45 -0600

    Add files via upload

---
+++ Data samples/preprocessing.R
@@ -0,0 +1,18 @@
+path = paste(getwd(), "/transactions.csv",sep="")
+file = read.csv(path)
+
+Months = c(0,31,60,91,121,152,182,213,244,274,305,335)
+
+timeStamps = c(); days = c()
+for(i in 1:nrow(file)){
+  t = as.character(file$transactionDateTime[i])
+  day = strsplit(t,"T")[[1]][1]
+  day = strsplit(day, '-')[[1]]
+  time = strsplit(t,"T")[[1]][2]
+  time = strsplit(time,":")[[1]]
+  days = c(days, Months[as.integer(day[2])] + as.integer(day[3]))
+  timeStamps = c(timeStamps, as.integer(time[1])/24+as.integer(time[2])/1440+as.integer(time[3])/86400)
+}
+
+file = data.frame(accountNumber=file$accountNumber, timeStamps=timeStamps, days=days)
+write.csv(file, file = "dataset.csv")
