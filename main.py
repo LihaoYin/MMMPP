c82e1660c541a6e357d4a388a30d6aa3a6a895e9
LihaoYin <86851749+LihaoYin@users.noreply.github.com>
2021-11-10 12:42:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from Dataset import *
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_path", type=str, default="datasets.csv", help="Path to training dataset")
-    parser.add_argument("--label_path", type=str, default="label.csv", help="Path to labels of training dataset")
+    parser.add_argument("--label_path", type=str, default="", help="Path to labels of training dataset")
     parser.add_argument("--test_dataset_path", type=str, default="test.csv", help="Path to testing dataset")
     parser.add_argument("--test_label_path", type=str, default="test.csv", help="Path to labels of testing dataset")
     parser.add_argument("--model", type=str, default="single_level", help="Type of model")
+    parser.add_argument("--naacounts", type=int, default=1000, help="Number of accounts")
     parser.add_argument("--mdays", type=int, default=1, help="Number of days")
     parser.add_argument("--event_types", type=int, default=1, help="Number of event types")
     parser.add_argument("--time_slot", type=tuple, default=(0,1), help="Time interval for each repeats")
@@ -19,6 +20,7 @@ if __name__ == "__main__":
         dataset_path = opt.dataset_path,
         label_path = opt.label_path,
         model = opt.model,
+        naccounts = opt.naccounts,
         mdays = opt.mdays
     )
 
